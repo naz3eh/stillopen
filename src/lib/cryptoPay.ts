216c7a4138ea6ac51bf857af2fa2b2416322344b
Nazeeh Vahora <nazeeh.xyz@gmail.com>
2026-09-15 21:30:18 +0530

    Add crypto pay constants for nazeeh.eth interim checkout

---
+++ src/lib/cryptoPay.ts
@@ -0,0 +1,5 @@
+/** Interim crypto checkout while Dodo Payments is pending. */
+export const CRYPTO_PAY_ENS = "nazeeh.eth";
+export const CRYPTO_PAY_ADDRESS = "0x0c12522fcda861460bf1bc223eca108144ee5df4";
+export const CRYPTO_PAY_AMOUNT_USD = 9;
+export const CRYPTO_PAY_ASSETS = "USDC (Ethereum) or ETH";
